0ed0dc5c033ff5e4a8479b9cc6fec9bd07f42c4e
annkim822@gmail.com Kim <annkim822@gmail.com>
2018-05-12 20:46:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var recipes = {};
+var recipes = {
+  maindish1 = "breakfast"
+  maindish2 = "lunch"
+  maindish3 = "dinner"
+};
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign ({}, object, { [key]: value })
@@ -10,5 +14,15 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+  delete recipes.maindish2
+  return recipes
 }
+
+var meals = { breakfast: “oatmeal”, lunch: “turkey sandwich”, dinner: “steak and potatoes” };
+
+   // the ‘delete’ operator returns ‘true’ if it has successfully
+   // deleted, ‘false’ otherwise
+   delete meals.dinner; // true
+
+   meals;
+   // returns { breakfast: “oatmeal”, lunch: “turkey sandwich” }
