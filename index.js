a71031dc96c73790ab02d9c0d1bff0184381bdd9
annkim822@gmail.com Kim <annkim822@gmail.com>
2018-05-12 20:49:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newRecipes = {Object.assign({}, object)}
+  var newRecipes = {food1, food2}
   delete newReceipes[key]
   return newReceipes
 }
